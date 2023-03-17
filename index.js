e5a8731679b47385cbc6da77729f5fe455c07d18
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2023-03-17 16:24:36 -0300

    Socket.io Server

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require ('express');
 const {json} = require("express");
 var app = express();
-var server = require('https').Server(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server, {
     cors: "*"
 });
-
-app.use(express.static('client'));
 
 app.get('/oi-mundo', function(req,res){
     res.status(200).send('Oi Mundo');
@@ -17,19 +15,8 @@ app.get('/:id', function(req,res){
     io.of("/").emit('messages', req.params.id);
 });
 
-/*var messages = [{
-    id: 1,
-    text: 'Bem-vindo ao chat privado de Socket.io e Node.js',
-    nickname: 'Bot - Marcelo diz:'
-}]*/
-
 io.on('connection', function(socket){
     console.log('O cliente com ip: '+socket.handshake.address+' se conectou');
-    /*socket.emit('messages', messages);
-    socket.on('add-message', function(data){
-        messages.push(data);
-        io.sockets.emit('messages',messages);
-    });*/
 });
 
 server.listen(3000, function(){
